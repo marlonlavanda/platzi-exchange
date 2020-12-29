d9bae2b32072450699f9a78ae676df659acccda6
Marlon Lavanda <marlonlavanda@hotmail.com>
2020-12-29 23:12:37 +0000

    Inicio del proyecto con Vue CLI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "@/assets/css/tailwind.css";
 
-Vue.config.productionTip = false
+import router from "@/router";
+import { dollarFilter, percentFilter } from "@/filters";
+
+Vue.filter("dollar", dollarFilter);
+Vue.filter("percent", percentFilter);
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
